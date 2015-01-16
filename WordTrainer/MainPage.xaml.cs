ae81c91cd515a402beda333e31541d3598d43718
Dmitri Soshnikov <dmitri@soshnikov.com>
2015-01-16 11:26:14 +0300

    Initial Commit

---
+++ WordTrainer/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WordTrainer.DataModel;
+
+// Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WordTrainer
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {        
+        public Word SelectedWord { get; set; }
+        public Word[] Words { get; set; }
+        public Word TheWord { get; set; }
+        
+        int Correct, Total;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NewWord();
+        }        
+        
+        protected void NewWord()
+        {
+            if (SelectedWord!= null)
+            {
+                if (SelectedWord.Name == TheWord.Name) Correct++;
+                Total++;
+            }
+            Words = Word.GetWordSample("*", 4);
+            TheWord = Helpers.Pick(Words);
+            tbWord.Text = TheWord.Name;
+            if (Total>0) tbResult.Text = string.Format("Correct={0}, Total={1}, Percent={2}%", Correct, Total, 100 * Correct / Total);
+            gvPics.SelectedItem = null;
+            gvPics.ItemsSource = Words;
+        }
+
+        private void gvPics_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedWord = gvPics.SelectedItem as Word;
+            NewWord();
+        }
+
+    }
+}
